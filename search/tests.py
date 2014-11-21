3a43c01709b4e43788f8c9ed4af628cdf98b0d8d
Tim Elam <tim@octane5.com>
2014-11-20 23:00:01 -0500

    [TESTS] Adds HTTP 200 check for main page.

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.test.client import Client
+
 
 # Create your tests here.
+class SearchPageTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_search_page_GET_200(self):
+        """A GET request for the search page should always return an HTTP 200 OK"""
+        # Issue a GET request.
+        from django.conf.global_settings import DATABASES
+        print(DATABASES)
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
