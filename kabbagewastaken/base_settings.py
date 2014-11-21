0d48db979cdbb4eefca6daeb1e18a63a2da65856
Tim Elam <tim@octane5.com>
2014-11-21 00:30:48 -0500

    [TASK] Adds bootstrap.css and search.css. Initial mock of search page.

--- kabbagewastaken/base_settings.py
+++ kabbagewastaken/base_settings.py
@@ -72,7 +72,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '..', 'static'),
 )
 
 TEMPLATE_DIRS = (
