39f12120bb5880c3b53c9e85e3d5389e0410da25
Tim Elam <tim@octane5.com>
2014-11-23 14:00:07 -0500

    [TASK] Prevents double-clicking search option button. Adds clarity to "both" button.

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,10 @@
 $(function(){
     $(".search-source-btn").click(function() {
         var $this = $(this);
+        $this.css('height', "");
+        $this.css('width', "");
+        // Disable this button
+        $this.prop('disabled', true);
         // Store original height of siblings for re-expansion
         $this.siblings().each(function(){
             $(this).data("height", $(this).css('height'));
@@ -10,7 +14,10 @@ $(function(){
         $this.siblings().animate(
             {'width': 0, 'height': 0},
             200,
-            function(){$(this).hide()}
+            function(){
+                $(this).hide();
+                $("#both-button").html("Twitter and Wikipedia");
+            }
         );
         // Show the "somewhere else" button
         $("#open-buttons").fadeIn(200).removeClass("hidden");
@@ -21,10 +28,12 @@ $(function(){
         // Re-open the search choice buttons
         $("#source-input").children().each(function() {
             var $this = $(this);
+            $this.prop("disabled", false);
             $this.show().animate(
                 {'width': $this.data('width'), 'height': $this.data('height')},
                 200
             );
         });
+        $("#both-button").html("both");
     });
 });
