acb9832a2084e4a95fa7b2b1569e3de4f98c90ec
Tim Elam <tim@octane5.com>
2014-11-24 12:39:03 -0500

    [FEATURE] Improves display of wikipedia links. Adds tests for twitter search, wikipedia search, and search form posting

--- search/tests.py
+++ search/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test.client import Client
-
+from .views import _search_twitter, _search_wikipedia
+from .forms import SearchForm
 
 # Create your tests here.
 class SearchPageTestCase(TestCase):
@@ -10,9 +11,40 @@ class SearchPageTestCase(TestCase):
     def test_search_page_GET_200(self):
         """A GET request for the search page should always return an HTTP 200 OK"""
         # Issue a GET request.
-        from django.conf.global_settings import DATABASES
-        print(DATABASES)
         response = self.client.get('/')
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
+
+    def test_search_post(self):
+        """A POST request to the search page"""
+
+        # A proper search on Twitter and Wikipedia
+        response = self.client.post('/', {'q': 'search', 'source': 'both'})
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # A search without source
+        response = self.client.post('/', {'q': 'search', 'source': ''})
+        # Check that the response is not a 200 OK.
+        self.assertNotEqual(response.status_code, 200)
+        # Check that the response is not a 500 INTERNAL SERVER ERROR.
+        self.assertNotEqual(response.status_code, 500)
+
+        # A Twitter search with no query
+        response = self.client.post('/', {'q': '', 'source': 'twitter'})
+        # Check that the response is a 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # A Wikipedia search
+        response = self.client.post('/', {'q': 'test', 'source': 'wikipedia'})
+        # Check that the response is a 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+    # Will raise an exception if improperly configured or service is down
+    def test_search_twitter(self):
+        _search_twitter("search")
+
+    # Will raise an exception if improperly configured or service is down
+    def test_search_wikipedia(self):
+        _search_wikipedia("search")
--- search/views.py
+++ search/views.py
@@ -8,6 +8,8 @@ from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.http.response import HttpResponse, HttpResponseBadRequest
 from .forms import SearchForm
+
+
 def search(request):
     if request.method == 'GET':
         return render_to_response('search/search.html', {}, RequestContext(request))
@@ -20,28 +22,20 @@ def search(request):
             has_results = False
             if source in ('twitter', 'both'):
                 # Search Twitter
-                consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
-                consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
-                access_token_key = os.environ.get("TWITTER_ACCESS_TOKEN")
-                access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
                 try:
-                    api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
-                    results_context['twitter_results'] = (
-                        [item['text'] for item in (api.request('search/tweets', {'q': form.cleaned_data['q']}))][:10]
-                    )
-                    has_results = True
+                    results_context['twitter_results'] = _search_twitter(form.cleaned_data['q'])
                 except:
                     results_context["twitter_error"] = "Twitter appears to be offline"
+                else:
+                    has_results = True
             if source in ('wikipedia', 'both'):
                 # Search Wikipedia
                 try:
-                    results_context['wiki_results'] = [item['snippet'] for item in json.load(urllib2.urlopen(
-                        "http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&srlimit=10&format=json".format(form.cleaned_data['q']))
-                    )['query']['search']]
-                    has_results = True
-                except Exception as e:
-                    print e
+                    results_context['wiki_results'] = _search_wikipedia(form.cleaned_data['q'])
+                except:
                     results_context["wiki_error"] = "Wikipedia appears to be offline"
+                else:
+                    has_results = True
             if has_results:
                 response_data['reason'] = render_to_string("search/results.html", results_context)
                 return HttpResponse(json.dumps(response_data), content_type="application/json")
@@ -49,8 +43,25 @@ def search(request):
                 response_data['reason'] = render_to_string("search/failure.html", results_context)
                 return HttpResponseBadRequest(json.dumps(response_data), content_type="application/json")
         else:
-            for field in form.fields:
-                print field.errors
             reason = 'Please select a search source'
             response_data['reason'] = render_to_string("search/failure.html", {'reason': reason})
             return HttpResponseBadRequest(json.dumps(response_data), content_type="application/json")
+
+def _search_twitter(query):
+    # Search Twitter
+    consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+    consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+    access_token_key = os.environ.get("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+    api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+    return [item['text'] for item in (api.request('search/tweets', {'q': query}))][:10]
+
+def _search_wikipedia(query):
+    query = "+".join(query.split())
+    return [
+        item for item in json.load(
+            urllib2.urlopen(
+                "http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&srlimit=10&format=json".format(query)
+            )
+        )['query']['search']
+    ]
